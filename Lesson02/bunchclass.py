e94c36bccf8d7b57d48e349c16d7aed00fe0e83c
James Pestrak <james.w.pestrak@gmail.com>
2014-10-01 23:49:36 -0700

    Ensure that methods are not over-ridden during initialization.

--- Lesson02/bunchclass.py
+++ Lesson02/bunchclass.py
@@ -1,10 +1,15 @@
 """
-Simple bunch class.
+Simple bunch class with a pretty printing method that protects its API.
 """
 
 class Bunch(object):
     def __init__(self, *args, **kwargs):
-        self.__dict__.update(kwargs)
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                raise AttributeError("API conflict: '%s' is part of '%s' API" % 
+                                     (key, self.__class__.__name__))
+            else:
+                setattr(self, key, value)
 
     def pretty(self):
         text = ""
